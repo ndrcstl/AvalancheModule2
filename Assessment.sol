7748efa0a89476defdb6b7ee23e4ce7081570a1f
ndrcstl <148858656+ndrcstl@users.noreply.github.com>
2024-07-02 20:32:44 +0800

    Update Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
-    event Transfer(uint256 amount, address recipient);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
@@ -39,7 +36,7 @@ contract Assessment {
     // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
+    function withdraw(uint256 _withdrawAmount) public payable {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
@@ -57,35 +54,17 @@ contract Assessment {
 
         // emit the event
         emit Withdraw(_withdrawAmount);
+
+        // send the amount to the owner
+        payable(owner).transfer(_withdrawAmount);
     }
 
-    // custom error
-    error InvalidRecipient(address recipient);
+    function checkOwner() public view returns (address) {
+        return owner;
+    }
 
-    function transfer(uint256 _amount, address _recipient) public {
+    function setNewOwner(address _newOwner) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        if (_recipient == address(0)) {
-            revert InvalidRecipient(_recipient);
-        }
-
-        uint _previousBalance = balance;
-        if (balance < _amount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _amount
-            });
-        }
-
-        // transfer the given amount
-        balance -= _amount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _amount));
-
-        // emit the event
-        emit Transfer(_amount, _recipient);
-
-        // send the amount to the recipient
-        payable(_recipient).transfer(_amount);
+        owner = payable(_newOwner);
     }
 }
