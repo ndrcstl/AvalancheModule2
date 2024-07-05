c398923285013ade87e9066580948ef854d637cc
ndrcstl <148858656+ndrcstl@users.noreply.github.com>
2024-07-06 01:29:04 +0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,137 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+
+export default function HomePage() {
+  const [ethWallet, setEthWallet] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
+  const [atm, setATM] = useState(undefined);
+  const [balance, setBalance] = useState(undefined);
+  const [newOwner, setNewOwnerAddress] = useState(""); // add new owner state
+
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const atmABI = atm_abi.abi;
+
+  const getWallet = async () => {
+    if (window.ethereum) {
+      setEthWallet(window.ethereum);
+    }
+
+    if (ethWallet) {
+      const account = await ethWallet.request({ method: "eth_accounts" });
+      handleAccount(account);
+    }
+  };
+
+  const handleAccount = (account) => {
+    if (account) {
+      console.log("Account connected: ", account);
+      setAccount(account);
+    } else {
+      console.log("No account found");
+    }
+  };
+
+  const connectAccount = async () => {
+    if (!ethWallet) {
+      alert('MetaMask wallet is required to connect');
+      return;
+    }
+
+    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
+    handleAccount(accounts);
+
+    // once wallet is set we can get a reference to our deployed contract
+    getATMContract();
+  };
+
+  const getATMContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethWallet);
+    const signer = provider.getSigner();
+    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
+
+    setATM(atmContract);
+  };
+
+  const getBalance = async () => {
+    if (atm) {
+      setBalance((await atm.getBalance()).toNumber());
+    }
+  };
+
+  const deposit = async () => {
+    if (atm) {
+      let tx = await atm.deposit(1);
+      await tx.wait()
+      getBalance();
+    }
+  };
+
+  const withdraw = async () => {
+    if (atm) {
+      let tx = await atm.withdraw(1);
+      await tx.wait()
+      getBalance();
+    }
+  };
+
+  const handleSetNewOwner = async () => {
+    if (atm) {
+      try {
+        let tx = await atm.setNewOwner(newOwner);
+        await tx.wait();
+        console.log("New owner set successfully!");
+      } catch (error) {
+        console.error("Error setting new owner:", error);
+      }
+    }
+  };
+
+  const initUser = () => {
+    // Check to see if user has Metamask
+    if (!ethWallet) {
+      return <p>Please install Metamask in order to use this ATM.</p>
+    }
+
+    // Check to see if user is connected. If not, connect to their account
+    if (!account) {
+      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+    }
+
+    if (balance == undefined) {
+      getBalance();
+    }
+
+    return (
+      <div>
+        <p>Your Account: {account}</p>
+        <p>Your Balance: {balance} ETH</p>
+        <button onClick={deposit}>Deposit 1 ETH</button>
+        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <input
+          type="text"
+          value={newOwner}
+          onChange={(e) => setNewOwnerAddress(e.target.value)}
+          placeholder="Enter new owner address"
+        />
+        <button onClick={handleSetNewOwner}>Set New Owner</button>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    getWallet();
+  }, []);
+
+  return (
+    <main className="container">
+      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      {initUser()}
+      <style jsx>{`
+       .container {
+          text-align: center
+        }
+      `}</style>
+    </main>
+  )
+}
